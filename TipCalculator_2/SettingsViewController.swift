8e4474a3da9bb19aa163d7e19fd8cdf11c933d0d
Xinyu Xieyang <yx4247@utexas.edu>
2019-01-11 03:40:55 -0600

    Tip&Split

--- TipCalculator_2/SettingsViewController.swift
+++ TipCalculator_2/SettingsViewController.swift
@@ -10,20 +10,61 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var DefaultTipControl: UISegmentedControl!
+    @IBOutlet weak var DefaultSplitField: UITextField!
+    
+    private var defaltTipChanged = false
+    private var defaltSplitChanged = false
+    private var defaultSplit = 1
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = UserDefaults.standard
+        
+        DefaultTipControl.selectedSegmentIndex = defaults.integer(forKey: "defaultTip")
+        defaultSplit = defaults.integer(forKey: "defaultSplit")
+        DefaultSplitField.placeholder = String(format: "%d", defaultSplit)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+        
+        if defaltTipChanged{
+            setDefaultTip()
+        }
+        
+        if defaltSplitChanged{
+            setDefaultSplit()
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeTipDefault(_ sender: Any) {
+        defaltTipChanged = true
     }
-    */
+    
+    @IBAction func changeSplitDefault(_ sender: Any) {
+        defaltSplitChanged = true
+        let split = Int(DefaultSplitField.text!) ?? defaultSplit
+        if split != defaultSplit {
+            defaultSplit = split
+        }
+    }
+    
+    func setDefaultTip(){
+        let defaults = UserDefaults.standard
+        defaults.set(DefaultTipControl.selectedSegmentIndex, forKey: "defaultTip")
+        
+        defaults.synchronize()
+    }
+    
+    func setDefaultSplit(){
+        let defaults = UserDefaults.standard
+        defaults.set(defaultSplit, forKey: "defaultSplit")
+        
+        defaults.synchronize()
+    }
 
 }
--- TipCalculator_2/ViewController.swift
+++ TipCalculator_2/ViewController.swift
@@ -16,23 +16,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var TipControl: UISegmentedControl!
     @IBOutlet weak var SplitField: UITextField!
     @IBOutlet weak var SplitLabel: UILabel!
+    private var splitDefault = 1
+    private var tipDefault = -1
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let splitDefault = 1
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = UserDefaults.standard
+        
+        TipControl.selectedSegmentIndex = defaults.integer(forKey: "defaultTip")
+        
+        splitDefault = defaults.integer(forKey: "defaultSplit")
         SplitField.placeholder = String(format: "%d", splitDefault)
-        // Do any additional setup after loading the view, typically from a nib.
+        SplitField.text = ""
+        calculateTip(NSNull())
+        
+        BillField.becomeFirstResponder()
     }
-
+    
+    @IBAction func editSplitField(_ sender: Any) {
+        SplitField.becomeFirstResponder()
+    }
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
-        let tipPercentage = [0.15, 0.20, 0.25]
+        let tipPercentage = [0.00, 0.15, 0.20, 0.25]
         
         let bill = Double(BillField.text!) ?? 0
-        let split = Int(SplitField.text!) ?? 1
+        let split = Int(SplitField.text!) ?? splitDefault
         let tip = bill * tipPercentage[TipControl.selectedSegmentIndex]
         
         let total = bill + tip
